46c3fe818aac3c0fb8aa9d31b5f637e784c390f3
DeAunte Hall <75832818+88-1982@users.noreply.github.com>
2022-03-12 12:12:19 -0800

    add connectionForm image and place holder to div class form-group

--- index.php
+++ index.php
@@ -8,3 +8,20 @@
 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Marvel Comics API Search - Character</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body onload="character()">
+  <div class="jumbotron">
+    <div class="container">
+      <h1 class="header-main-title">Marvel Comics Character Search</h1>
+      <form id="connectionForm">
+        <div class="form-group">
+        <img src="https://wallpaperaccess.com/full/1869517.jpg" alt="marvel-heroes" class="center" width="1000">
+          <input required type="text" name="name" id="name" class="form-control character-search-box"
+            placeholder="(Enter Hero Here )">
+        </div>
+        <input type="submit" value="Search!" class="btn btn-danger mb-2 float-right search-character-button">
+      </form>
+    </div>
+  </div>
