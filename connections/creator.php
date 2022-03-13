5b55e2c695c51ad6a4e2752822b8247d2177fb95
DeAunte Hall <75832818+88-1982@users.noreply.github.com>
2022-03-13 03:22:34 -0700

    add keys to file

--- connections/creator.php
+++ connections/creator.php
@@ -1,8 +1,39 @@
 <?php
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest')) {
+if (isset($_GET['creator-id'])) {
 
+  $curl = curl_init();
 
-  if (isset($_GET['creator-id'])){
-    
-  }
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+  $creator_id = htmlentities(strtolower($_GET['creator-id'])); //FJEkejfdkf = fjekjfdkf
+
+  $ts = time();
+  $public_key = '301c1f60ac74082c6c8342e5538109c5';
+  $private_key = 'c2be6028db99ef63108939ee427881d051514553
+  ';
+  $hash = md5($ts . $private_key . $public_key);
+
+  $query = array(
+  'apikey' => $public_key,
+  'ts' => $ts,
+  'hash' => $hash,
+  );
+
+  curl_setopt($curl, CURLOPT_URL,
+  "https://gateway.marvel.com:443/v1/public/creators/" . $creator_id . "?" . http_build_query($query)
+  );
+
+  $result = json_decode(curl_exec($curl), true);
+
+  curl_close($curl);
+
+  echo json_encode($result);
+
+} else {
+  echo "Error comic-id invalid.";
 }
+} else {
+echo "Error: wrong server.";
+}
+?>
