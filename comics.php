9b3842f96cefe5f1fe4ca879151b4dc5e5cca690
DeAunte Hall <75832818+88-1982@users.noreply.github.com>
2022-03-12 19:18:01 -0800

    update title

--- comics.php
+++ comics.php
@@ -8,7 +8,7 @@
   
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <title>Marvel Comics API Search - Comic</title>
+  <title>Marvel Comics Search - Comic</title>
   <link rel="stylesheet" href="style.css">
 </head>
 <body onload="singleComic()" class="single-comic__body">
