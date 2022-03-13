4ef7944c27a7be5218f420963e2ea0933d68f23f
DeAunte Hall <75832818+88-1982@users.noreply.github.com>
2022-03-13 03:04:26 -0700

    add API keys to file

--- connections/name-search.php
+++ connections/name-search.php
@@ -4,46 +4,36 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-  
-  $name_to_search = htmlentities(strtolower($_GET['name']));
+    $name_to_search = htmlentities(strtolower($_GET['name'])); 
 
+    $ts = time();
+    $public_key = '301c1f60ac74082c6c8342e5538109c5';
+  $private_key = 'c2be6028db99ef63108939ee427881d051514553
+  ';
+    $hash = md5($ts . $private_key . $public_key);
 
-  $ts = time();
-  $public_key = 'c2be6028db99ef63108939ee427881d051514553';
-  $private_key = '301c1f60ac74082c6c8342e5538109c5';
-  $hash = md5($ts . $private_key. $public_key);
+    $query = array(
+      "name" => $name_to_search, 
+      "orderBy" => "name",
+      "limit" => "20",
+      'apikey' => $public_key,
+      'ts' => $ts,
+      'hash' => $hash,
+    );
 
-  
+    $marvel_url = 'https://gateway.marvel.com:443/v1/public/characters?' . http_build_query($query);
 
+    curl_setopt($curl, CURLOPT_URL, $marvel_url);
 
+    $result = json_decode(curl_exec($curl), true);
 
-$query = array(
-  "name" =>$name_to_search,
-  "orderby" => "name",
-  "limit" => "40",
-  'apikey' => $public_key,
-  'ts' => $ts,
-  'hash' => $hash
-);
+    curl_close($curl);
 
-  $comic_url = 'https://gateway.marvel.com:443/v1/public/characters?' .http_build_query($query);
-  
-  $curl_setopt($curl, CURLOPT_URL, $comic_url);
-  
-  
-  $result = json_decode (curl_exec($curl), true);
+    echo json_encode($result);
 
-  $curl_close($curl);
-
-  echo json_encode($result);
-
-} else {
-  echo "Error: Add Name...";
-
-}
-
-
-
+  } else {
+    echo "Error: no name given.";
+  }
 } else {
   echo "Error: wrong server.";
 }
